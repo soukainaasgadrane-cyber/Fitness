8feee283492cf74e8bd2703a7f444da506b49eba
soukaina asg <soukainaasgadrane@gmail.com>
2026-03-25 00:45:04 +0100

    first commit

---
+++ database/migrations/2026_03_24_234346_create_members_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->string('address')->nullable();
+            $table->date('membership_start_date');
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('members');
+    }
+};
